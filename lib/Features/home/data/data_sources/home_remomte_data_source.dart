63010c54e476de252d26e778eb3717c7e52fa8ee
MagdKamaldev <magddevf@gmail.com>
2023-08-08 08:30:08 +0300

    implement fetch featured books

---
+++ lib/Features/home/data/data_sources/home_remomte_data_source.dart
@@ -0,0 +1,35 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import '../../../../core/utils/api_services.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fecthFeaturedBooks();
+  Future<List<BookEntity>> fecthNewestBooks();
+}
+
+class HomeRemoteDataSourceImplementation extends HomeRemoteDataSource {
+  final ApiService apiService;
+  HomeRemoteDataSourceImplementation(this.apiService);
+
+  @override
+  Future<List<BookEntity>> fecthFeaturedBooks() async {
+    var data = await apiService.get(endpoint: "volumes?q=computer science");
+  
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+      List<BookEntity> books = [];
+    for (var bookMap in data["items"]) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> fecthNewestBooks() {
+    // TODO: implement fecthNewestBooks
+    throw UnimplementedError();
+  }
+}
