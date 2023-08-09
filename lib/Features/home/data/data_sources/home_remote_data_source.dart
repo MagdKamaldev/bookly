149219567faf7ea0252aa5f2fd751b730c4885f0
MagdKamaldev <magddevf@gmail.com>
2023-08-09 09:29:00 +0300

    cache featured books

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/constants.dart';
 import '../../../../core/utils/api_services.dart';
+import '../../../../core/utils/functions/save_books.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fecthFeaturedBooks();
@@ -13,28 +15,32 @@ class HomeRemoteDataSourceImplementation extends HomeRemoteDataSource {
 
   @override
   Future<List<BookEntity>> fecthFeaturedBooks() async {
-    var data = await apiService.get(endpoint: "volumes?q=computer science&Filtering=free-ebooks&key=AIzaSyAxT34xJRaWTN84cubUJqFs-CoN9HjUzPc");
-  
+    var data = await apiService.get(
+        endpoint:
+            "volumes?q=computer science&Filtering=free-ebooks&key=AIzaSyAxT34xJRaWTN84cubUJqFs-CoN9HjUzPc");
+
     List<BookEntity> books = getBooksList(data);
+    saveBooksData(books,kFeaturedBox);
     return books;
   }
 
-  
+
+
   @override
-  Future<List<BookEntity>> fecthNewestBooks() async{
-     var data = await apiService.get(endpoint: "volumes?q=computer science&Filtering=free-ebooks&Sorting=newest&key=AIzaSyAxT34xJRaWTN84cubUJqFs-CoN9HjUzPc");
-  
+  Future<List<BookEntity>> fecthNewestBooks() async {
+    var data = await apiService.get(
+        endpoint:
+            "volumes?q=computer science&Filtering=free-ebooks&Sorting=newest&key=AIzaSyAxT34xJRaWTN84cubUJqFs-CoN9HjUzPc");
+
     List<BookEntity> books = getBooksList(data);
     return books;
-  
   }
 
   List<BookEntity> getBooksList(Map<String, dynamic> data) {
-      List<BookEntity> books = [];
+    List<BookEntity> books = [];
     for (var bookMap in data["items"]) {
       books.add(BookModel.fromJson(bookMap));
     }
     return books;
   }
-
 }
